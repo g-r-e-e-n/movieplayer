808e47c6215a5749e0a578a17459b554e7ff7592
g-r-e-e-n <90210015+g-r-e-e-n@users.noreply.github.com>
2021-09-16 01:02:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b alpha https://github.com/g-r-e-e-n/movieplayer.git /movieplayer
+git clone ttps://github.com/g-r-e-e-n/movieplayer.git /movieplayer
 cd /movieplayer
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
